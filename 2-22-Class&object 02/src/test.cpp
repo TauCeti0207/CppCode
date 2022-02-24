562d370b5c11753f6b8a3e5a026c65c7431945c1
TauCeti0207 <2076188013@qq.com>
2022-02-24 21:30:06 +0800

    2-24 复习c进阶

--- 2-22-Class&object 02/src/test.cpp
+++ 2-22-Class&object 02/src/test.cpp
@@ -68,20 +68,20 @@ istream &operator>>(istream &in, Date &d)
     in >> d._year >> d._month >> d._day;
     return in;
 }
-int main(int argc, char const *argv[])
-{
-    Date d1(2022, 2, 22);
-    Date d2(2022, 2, 22);
-    // cout << (d1 == d2) << endl;
-    // d1.Fun1();
-    // cout << &d1 << endl;
-    // &d2;
-    cin >> d1 >> d2;
-    cout << d1 << endl
-         << d2 << endl;
-    system("pause");
-    return 0;
-}
+// int main(int argc, char const *argv[])
+// {
+//     Date d1(2022, 2, 22);
+//     Date d2(2022, 2, 22);
+//     // cout << (d1 == d2) << endl;
+//     // d1.Fun1();
+//     // cout << &d1 << endl;
+//     // &d2;
+//     cin >> d1 >> d2;
+//     cout << d1 << endl
+//          << d2 << endl;
+//     system("pause");
+//     return 0;
+// }
 
 // int main(int argc, char const *argv[])
 // {
@@ -90,3 +90,90 @@ int main(int argc, char const *argv[])
 //     system("pause");
 //     return 0;
 // }
+// #include <stdio.h>
+// int main()
+// {
+//     unsigned char a = 200;
+//     unsigned char b = 100;
+//     unsigned char c = 0;
+//     c = a + b;
+//     printf("%d %d", a + b, c); //300 44
+//     system("pause");
+// }
+// 1
+// 1 1
+// 1 2 1
+// 1 3 3 1
+// 1 4 6 4 1
+// #include<stdio.h>
+// //转换为二维数组问题，第一列都对角线都是1
+// int main()
+// {
+// 	int arr[10][10] = { 0 };
+// 	int i = 0;
+// 	int j = 0;
+// 	for (i = 0; i < 10; i++)
+// 	{
+//         for(int k = 0; k < 10-i; k++)
+//             {
+//                 printf(" ");
+//             }
+// 		for (j = 0; j <= i; j++)
+// 		{
+// 			if (j == 0)//第一列
+// 				arr[i][j] = 1;
+// 			if (i == j)//对角线
+// 				arr[i][j] = 1;
+// 			if (i >= 2 && j >= 1)
+// 				arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+// 			printf("%2d ", arr[i][j]);
+// 		}
+// 		printf("\n");
+// 	}
+//     system("pause");
+// 	return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     int arr[10] = {0};
+//     printf("arr = %p\n", arr);
+//     printf("&arr = %p\n", &arr);
+
+//     printf("&(arr[0]) = %p\n", &(arr[0]));
+//     printf(" &(arr[0])+1) = %p\n", &(arr[0]) + 1);
+
+//     printf("arr+1 = %p\n", arr + 1);
+//     printf("&arr+1= %p\n", &arr + 1);
+//     system("pause");
+//     return 0;
+// }
+// // arr = 0000001bac7ffd00
+// // &arr = 0000001bac7ffd00
+// // &(arr[0]) = 0000001bac7ffd00
+// //  &(arr[0])+1) = 0000001bac7ffd04
+// // arr+1 = 0000001bac7ffd04
+// // &arr+1= 0000001bac7ffd28
+
+#include <stdio.h>
+int main()
+{
+    int arr[10] = {0};
+    printf("arr = %p\n", arr);
+    printf("arr+1 = %p\n", arr + 1);
+
+    printf("&(arr[0]) = %p\n", &(arr[0]));
+    printf(" &(arr[0])+1) = %p\n", &(arr[0]) + 1);
+
+    printf("arr+1 = %p\n", arr + 1);
+    printf("&arr+1= %p\n", &arr + 1);
+    system("pause");
+    return 0;
+}
+// arr = 000000e0f57ffd20
+// arr+1 = 000000e0f57ffd24
+// &(arr[0]) = 000000e0f57ffd20
+//  &(arr[0])+1) = 000000e0f57ffd24
+// arr+1 = 000000e0f57ffd24
+// &arr+1= 000000e0f57ffd48
