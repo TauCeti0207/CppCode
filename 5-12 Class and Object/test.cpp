351042fa3bbcfdb33244e2575cdca6ecf84fcbd9
TauCeti0207 <2076188013@qq.com>
2022-05-14 22:56:48 +0800

    5-13 C++ class and object

--- 5-12 Class and Object/test.cpp
+++ 5-12 Class and Object/test.cpp
@@ -74,38 +74,126 @@ using namespace std;
  //封装：更严格管理设计
  //数据和方法封装到一起，放到类里面。
 
-class Stack
+//class Stack
+//{
+//private:
+//	void Checkcapaicty()
+//	{}
+//public:
+//	void Init()
+//	{}
+//
+//	void Push(int x)
+//	{}
+//
+//	int Top()
+//	{}
+//
+//private:
+//	int* _a;
+//	int _top;
+//	int _capacity;
+//};
+//
+//int main()
+//{
+//	Stack st;
+//	st.Init();
+//	st.Push(1);
+//	st.Push(2);
+//	st.Push(3);
+//	st.Push(4);
+//
+//	cout << st.Top() << endl;
+//	//cout << st._a[st._top] << endl; // 无法访问私有成员变量
+//	//C++强制规定了用户使用规范。
+//	return 0;
+//}
+
+//typedef int STDataType;
+//
+//class Stack
+//{
+//public:
+//	void Init(int initSize = 4);
+//	void Push(STDataType x);
+//
+//	// ...
+//private:
+//	STDataType* _a;
+//	int _size;
+//	int _capacity;
+//};
+//void Stack::Init(int initSize)
+//{
+//	_a = (STDataType*)malloc(sizeof(STDataType) * initSize);
+//	_size = 0;
+//	_capacity = initSize;
+//}
+//
+//void Stack::Push(STDataType x)
+//{
+//	// ...
+//	_a[_size] = x;
+//	_size++;
+//}
+//int main()
+//{
+//	//struct Stack st;
+//	Stack st;
+//	st.Init();
+//	st.Push(1);
+//	st.Push(2);
+//	st.Push(3);
+//
+//	Stack st2;
+//	st2.Init(); // 和st调用的是同一个函数
+//	cout << sizeof(st) << endl;// 12 
+//	cout << sizeof(st2) << endl;// 12 32位环境下 结构体总大小为最大对齐数的整数倍
+//	return 0;
+//}
+
+
+
+class A1 
 {
-private:
-	void Checkcapaicty()
-	{}
 public:
-	void Init()
-	{}
-
-	void Push(int x)
-	{}
-
-	int Top()
-	{}
-
+	void f1() {}
 private:
-	int* _a;
-	int _top;
-	int _capacity;
+	int _a; // 占了0~3
+	char _x; // char最小对齐是1，占用4
+	// VS默认对齐是8，结构体总大小为最大对齐数（每个成员变量都有一个对齐数）的整数倍
+	// 现在最大对齐数是4，因此总的大小就是8
 };
 
+//空类
+class A2
+{};
+
+//类中仅有成员函数
+class A3
+{
+public:
+	void f2() {}
+};
+void test2()
+{
+	A1 a1;
+	A2 a2;
+	A3 a3;
+	cout << sizeof(A1) << endl;// 8 注意内存对齐：VS默认对齐为8
+	cout << sizeof(a1) << endl;// 8
+	cout << sizeof(A2) << endl;// 1
+	cout << sizeof(A3) << endl;// 1
+	cout << sizeof(a2) << endl;// 1
+	cout << sizeof(a3) << endl;// 1
+
+	cout << &a1 << endl;
+	cout << &a2 << endl;
+	cout << &a3 << endl;
+}
 int main()
 {
-	Stack st;
-	st.Init();
-	st.Push(1);
-	st.Push(2);
-	st.Push(3);
-	st.Push(4);
-
-	cout << st.Top() << endl;
-	//cout << st._a[st._top] << endl; // 无法访问私有成员变量
-	//C++强制规定了用户使用规范。
+	test2();
 	return 0;
 }
